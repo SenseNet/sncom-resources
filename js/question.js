bc33ccc602071891935022a2fc0727fe9a79f9ce
Pranshu Chittora <pranshuchittora17@gmail.com>
2019-10-06 22:56:05 +0530

    chore: updates the question.js to ES6
    ES6 + Formatting

--- js/question.js
+++ js/question.js
@@ -1,32 +1,36 @@
 function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+  const name = cname + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
     }
-    return "";
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
 
-$(window).on('load', function (){
-	var c = getCookie('openquestion');
-	if ( c == null || c == false) {
-          setTimeout (function(){
-              $('.js-slide_block').addClass('open');
-          }, 3000);
-    }
-	
-	$('.js-slide_block__close').on('click', function () {
-          $(this).closest('.js-slide_block').removeClass('open');
-	});
+$(window).on("load", function() {
+  const c = getCookie("openquestion");
+  if (c == null || c == false) {
+    setTimeout(function() {
+      $(".js-slide_block").addClass("open");
+    }, 3000);
+  }
 
-	$('.js-go_to_survey').on('click', function () {
-		$(this).closest('.js-slide_block').removeClass('open');
-	});
+  $(".js-slide_block__close").on("click", function() {
+    $(this)
+      .closest(".js-slide_block")
+      .removeClass("open");
+  });
+
+  $(".js-go_to_survey").on("click", function() {
+    $(this)
+      .closest(".js-slide_block")
+      .removeClass("open");
+  });
 });
